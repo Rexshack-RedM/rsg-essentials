4265e380919b1f2367141a03e3c693e243e58a61
RexShack <rexshackuk@gmail.com>
2022-12-11 07:05:41 +0000

    Update handsup.lua

--- client/handsup.lua
+++ client/handsup.lua
@@ -2,27 +2,24 @@ local QRCore = exports['qr-core']:GetCoreObject()
 
 CreateThread(function()
     while true do
-        Citizen.Wait(7)
-        if IsControlJustPressed(0, QRCore.Shared.Keybinds['X']) then -- x
-            local playerPed = PlayerPedId()
-            if not IsEntityDead(playerPed) and not Citizen.InvokeNative(0x9682F850056C9ADE, playerPed) then
+        Wait(7)
+        if IsControlJustPressed(0, QRCore.Shared.Keybinds['X']) then
+            local ped = PlayerPedId()
+            if not IsEntityDead(ped) and not Citizen.InvokeNative(0x9682F850056C9ADE, ped) then
                 local animDict = "script_proc@robberies@homestead@lonnies_shack@deception"
-
-                if not IsEntityPlayingAnim(playerPed, animDict, "hands_up_loop", 3) then
+                if not IsEntityPlayingAnim(ped, animDict, "hands_up_loop", 3) then
                     if not HasAnimDictLoaded(animDict) then
                         RequestAnimDict(animDict)
-
                         while not HasAnimDictLoaded(animDict) do
-                            Citizen.Wait(0)
+                            Wait(0)
                         end
                     end
-
-                    TaskPlayAnim(playerPed, animDict, "hands_up_loop", 2.0, -2.0, -1, 67109393, 0.0, false, 1245184, false, "UpperbodyFixup_filter", false)
+                    TaskPlayAnim(ped, animDict, "hands_up_loop", 2.0, -2.0, -1, 67109393, 0.0, false, 1245184, false, "UpperbodyFixup_filter", false)
                     RequestAnimDict(animDict)
                 else
-                    ClearPedSecondaryTask(playerPed)
+                    ClearPedSecondaryTask(ped)
                 end
             end
         end
-    end        
+    end
 end)
