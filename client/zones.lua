b98de56cd66e5c124c81bd3628b2706807bc922d
RexshackGaming <rexshackuk@gmail.com>
2023-02-21 18:56:22 +0000

    add credit to Viyu88

--- client/zones.lua
+++ client/zones.lua
@@ -1,4 +1,4 @@
--- add map zones
+-- add map zones : credit to Viyu88 https://github.com/Viyu88/viyu_colormap
 Citizen.CreateThread(function()
     for v = 1, #Config.Zones, 1 do
         Citizen.InvokeNative(0x563FCB6620523917, Config.Zones[v].Hash, GetHashKey(Config.Zones[v].Color))
