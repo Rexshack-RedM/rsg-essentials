3ad6abd73a31bcae1620011ccf5eb14870ed3a2d
RexshackGaming <rexshackuk@gmail.com>
2023-03-03 18:52:46 +0000

    change voice proximity

--- client/smallfixes.lua
+++ client/smallfixes.lua
@@ -1,8 +1,20 @@
+local RSGCore = exports['rsg-core']:GetCoreObject()
+
 -- ped drown / boat sink fix
 Citizen.CreateThread(function()
     while true do
         Wait(0)
         Citizen.InvokeNative(0xC1E8A365BF3B29F2, PlayerPedId(), 364)   -- PRF_IgnoreDrownAndKillVolumes
+    end
+end)
+
+-- change voice proximity
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        if IsControlJustPressed(0, RSGCore.Shared.Keybinds['RIGHTBRACKET']) then
+            ExecuteCommand('cycleproximity')
+        end
     end
 end)
 
