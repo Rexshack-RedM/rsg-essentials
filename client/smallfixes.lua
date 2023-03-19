c88100a52096da40f9dd957dc8b994e5f3f4cb00
RexshackGaming <rexshackuk@gmail.com>
2023-03-19 11:19:53 +0000

    Update smallfixes.lua

--- client/smallfixes.lua
+++ client/smallfixes.lua
@@ -1,4 +1,5 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
+local PauseOpen = false
 
 -- ped drown / boat sink fix
 Citizen.CreateThread(function()
@@ -46,5 +47,29 @@ Citizen.CreateThread(function()
             end
         end
         Wait(1000)
+    end
+end)
+
+-- pause menu animation thanks to ManLikeTJB#4518
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        local ped = PlayerPedId()
+        if IsPauseMenuActive() and not PauseOpen then
+            SetCurrentPedWeapon(ped, 0xA2719263, true) -- set unarmed
+            SetCurrentPedWeapon(ped, GetHashKey("weapon_unarmed"))
+            if not IsPedOnMount(ped) then
+                TaskStartScenarioInPlace(PlayerPedId(), GetHashKey("WORLD_HUMAN_SIT_GROUND_READING_BOOK"), -1, true, "StartScenario", 0, false)
+            end
+            PauseOpen = true
+        end
+
+        if not IsPauseMenuActive() and PauseOpen then
+            ClearPedTasks(ped)
+            Wait(4000)
+            SetCurrentPedWeapon(ped, 0xA2719263, true) -- set unarmed
+            SetCurrentPedWeapon(ped, GetHashKey("weapon_unarmed"))
+            PauseOpen = false
+        end
     end
 end)
