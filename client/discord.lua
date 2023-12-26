55331ec1b60ec4238e3715e2d166ec5370068fdc
Tek <154287313+TekJeek@users.noreply.github.com>
2023-12-26 13:41:48 +0100

    Update discord.lua

--- client/discord.lua
+++ client/discord.lua
@@ -1,66 +1,22 @@
-local discordAppId  = Config.Discord["discord_id"]
-local joinUrl       = Config.Discord["discord_joinurl"]
-local website       = Config.Discord["discord_connecturl"]
-
-local isRDR = not TerraingridActivate
-
-local function getZoneName(coords, type)
-	local zoneHash = Citizen.InvokeNative(0x43AD8FC02B429D33, coords.x, coords.y, coords.z, type)
-	return zoneNames[zoneHash]
-end
-
 Citizen.CreateThread(function()
-	while true do
-		local playerPed = PlayerPedId()
-		local playerName = GetPlayerName(PlayerId())
-		local playerCoords = GetEntityCoords(playerPed)
-		local health = GetEntityHealth(playerPed)
+    SetDiscordAppId(Config.Discord.DiscordAppID)
 
-		SetDiscordAppId(discordAppId)
+    SetDiscordRichPresenceAsset(Config.Discord.AppBigAssetID)
+    SetDiscordRichPresenceAssetText(Config.Discord.AppBigAssetText)
 
-		local location
+    SetDiscordRichPresenceAssetSmall(Config.Discord.AppSmallAssetID)
+    SetDiscordRichPresenceAssetSmallText(Config.Discord.AppSmallAssetText)
 
-		if isRDR then
-			local town = getZoneName(playerCoords, 1)
-			local district = getZoneName(playerCoords, 10)
-			local state = getZoneName(playerCoords, 0)
+    SetDiscordRichPresenceAction(0, Config.Discord.FirstButtonPlaceholder, Config.Discord.FirstButtonLink)
+    SetDiscordRichPresenceAction(1, Config.Discord.SecondButtonPlaceholder, Config.Discord.SecondButtonLink)
 
-			if town then
-				location = town
-			elseif district then
-				location = district
-			elseif state then
-				location = state
-			else
-				location = "Unknown"
-			end
-		else
-			local zone = GetLabelText(GetNameOfZone(playerCoords))
+    while true do
+        TriggerServerEvent('rsg-discord:getdata')
 
-			local street, crossing = GetStreetNameAtCoord(playerCoords.x, playerCoords.y, playerCoords.z)
-			local streetName = GetStreetNameFromHashKey(street)
-			local crossingName = GetStreetNameFromHashKey(crossing)
-			local road
-			if crossingName ~= "" then
-				road = streetName .. " & " .. crossingName
-			else
-				road = streetName
-			end
+        Citizen.Wait(Config.Discord.UpdateEvery)
+    end
+end)
 
-			loation = road .. ", " .. zone
-		end
-
-		SetRichPresence(location)
-
-		SetDiscordRichPresenceAsset(Config.Discord["discord_big_image"])
-		SetDiscordRichPresenceAssetText(playerName)
-
-		SetDiscordRichPresenceAssetSmall(Config.Discord["discord_small_image"])
-		SetDiscordRichPresenceAssetSmallText("Health: " .. health)
-
-		SetDiscordRichPresenceAction(0, "Discord", joinUrl)
-		SetDiscordRichPresenceAction(1, "Website", website)
-
-		Citizen.Wait(5000)
-	end
+RegisterNetEvent("rsg-discord:receivedata", function(richPresenceString)
+    SetRichPresence(richPresenceString)
 end)
