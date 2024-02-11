f9284feb76532d138ccbc27fa8a56e276e30beea
RexshackGaming <rexshackuk@gmail.com>
2024-02-11 08:12:44 +0000

    menu to ox_lib

--- client/emotes.lua
+++ client/emotes.lua
@@ -1,4 +1,6 @@
--- [[ Exports ]] --
+-------------------------------------------------------
+-- exports
+-------------------------------------------------------
 local function doemote(emote)
     Citizen.InvokeNative(0xB31A277C1AC7B7FF, PlayerPedId(), 0, 0, emote, 1, 1, 0, 0)
 end exports('doemote', doemote)
@@ -11,7 +13,9 @@ local function dodictemote(dict, anim, duration)
     Citizen.InvokeNative(0xEA47FE3719165B94, PlayerPedId(), dict, anim, 8.0, -8.0, duration, 1, 0, true, 0, false, 0, false)
 end exports('dodictemote', dodictemote)
 
--- [[ Events ]] --
+-------------------------------------------------------
+-- events
+-------------------------------------------------------
 RegisterNetEvent("emotes:client:doemote")
 AddEventHandler("emotes:client:doemote", function(data)
     Citizen.InvokeNative(0xB31A277C1AC7B7FF, PlayerPedId(), 0, 0, data, 1, 1, 0, 0)
@@ -32,153 +36,177 @@ AddEventHandler("emotes:client:dodictemote", function(data)
     Citizen.InvokeNative(0xEA47FE3719165B94, ped, data.dict, data.anim, 8.0, -8.0, data.duration, 1, 0, true, 0, false, 0, false)
 end)
 
--- [[ Emote Menu ]] --
-
+-------------------------------------------------------
+-- main emote menu
+-------------------------------------------------------
 RegisterNetEvent('emotes:client:EmoteMenu', function()
-    exports['rsg-menu']:openMenu({{
-        header = Lang:t('emotes.title'),
-        isMenuHeader = true -- Set to true to make a nonclickable title
-    }, {
-        header = Lang:t('emotes.actions.mainMenu'),
-        params = {
-            event = 'emotes:client:actionemotes',
+    lib.registerContext(
+        {
+            id = 'emotes_mainmenu',
+            title = Lang:t('emotes.title'),
+            position = 'top-right',
+            options = {
+                {
+                    title = Lang:t('emotes.actions.mainMenu'),
+                    icon = 'fa-solid fa-icons',
+                    event = 'emotes:client:actionemotes'
+                },
+                {
+                    title = Lang:t('emotes.greeting.mainMenu'),
+                    icon = 'fa-solid fa-icons',
+                    event = 'emotes:client:greetingemotes'
+                },
+                {
+                    title = Lang:t('emotes.reaction.mainMenu'),
+                    icon = 'fa-solid fa-icons',
+                    event = 'emotes:client:reactionemotes'
+                },
+                {
+                    title = Lang:t('emotes.taunting.mainMenu'),
+                    icon = 'fa-solid fa-icons',
+                    event = 'emotes:client:tauntemotes'
+                },
+                {
+                    title = Lang:t('emotes.dancing.mainMenu'),
+                    icon = 'fa-solid fa-icons',
+                    event = 'emotes:client:dancingemotes'
+                },
+            }
         }
-    }, {
-        header = Lang:t('emotes.greeting.mainMenu'),
-        params = {
-            event = 'emotes:client:greetemotes'
-        }
-    }, {
-        header = Lang:t('emotes.reaction.mainMenu'),
-        params = {
-            event = 'emotes:client:reactionemotes'
-        }
-    },{
-        header = Lang:t('emotes.taunting.mainMenu'),
-        params = {
-            event = 'emotes:client:tauntemotes'
-        }
-    },{
-        header = Lang:t('emotes.close'),
-        params = {
-            event = 'emotes:client:danceemotes'
-        }
-    }})
+    )
+    lib.showContext('emotes_mainmenu')
 end)
 
+-------------------------------------------------------
+-- actions emote menu
+-------------------------------------------------------
 RegisterNetEvent('emotes:client:actionemotes', function()
-    Citizen.Wait(500)
-    local EmoteMenu = {{
-        header = Lang:t('emotes.actions.secondMenu'),
-        isMenuHeader = true -- Set to true to make a nonclickable title
-    }, {
-        header = Lang:t('emotes.close'),
-    }}
-    for k, v in pairs(Config.Emotes.Actions) do
-        EmoteMenu[#EmoteMenu + 1] = {
-            header = "/"..k,
-            txt = v.desc,
-            params = {
-                event = 'emotes:client:doemotemenu',
-                args = {
-                    emote = v.anim,
-                }
-            }
+    local options = {}
+    for k,v in pairs(Config.Emotes.Actions) do
+        options[#options + 1] = {
+            title = "/"..k,
+            description = v.desc,
+            icon = 'fa-solid fa-icons',
+            event = 'emotes:client:doemotemenu',
+            args = { emote = v.anim },
+            arrow = false,
         }
     end
-    exports['rsg-menu']:openMenu(EmoteMenu)
+    lib.registerContext({
+        id = 'emotes_actionsmenu',
+        title = Lang:t('emotes.actions.secondMenu'),
+        menu = 'emotes_mainmenu',
+        onBack = function() end,
+        position = 'top-right',
+        options = options
+    })
+    lib.showContext('emotes_actionsmenu')
 end)
 
-RegisterNetEvent('emotes:client:greetemotes', function()
-    Citizen.Wait(500)
-    local EmoteMenu = {{
-        header = Lang:t('emotes.greeting.secondMenu'),
-        isMenuHeader = true -- Set to true to make a nonclickable title
-    }, {
-        header = Lang:t('emotes.close'),
-    }}
-    for k, v in pairs(Config.Emotes.Greeting) do
-        EmoteMenu[#EmoteMenu + 1] = {
-            header = "/"..k,
-            txt = v.desc,
-            params = {
-                event = 'emotes:client:doemotemenu',
-                args = {
-                    emote = v.anim,
-                }
-            }
+-------------------------------------------------------
+-- greetings emote menu
+-------------------------------------------------------
+RegisterNetEvent('emotes:client:greetingemotes', function()
+    local options = {}
+    for k,v in pairs(Config.Emotes.Greeting) do
+        options[#options + 1] = {
+            title = "/"..k,
+            description = v.desc,
+            icon = 'fa-solid fa-icons',
+            event = 'emotes:client:doemotemenu',
+            args = { emote = v.anim },
+            arrow = false,
         }
     end
-    exports['rsg-menu']:openMenu(EmoteMenu)
+    lib.registerContext({
+        id = 'emotes_greetingmenu',
+        title = Lang:t('emotes.greeting.secondMenu'),
+        menu = 'emotes_mainmenu',
+        onBack = function() end,
+        position = 'top-right',
+        options = options
+    })
+    lib.showContext('emotes_greetingmenu')
 end)
 
+-------------------------------------------------------
+-- reaction emote menu
+-------------------------------------------------------
 RegisterNetEvent('emotes:client:reactionemotes', function()
-    Citizen.Wait(500)
-    local EmoteMenu = {{
-        header = Lang:t('emotes.reaction.secondMenu'),
-        isMenuHeader = true -- Set to true to make a nonclickable title
-    }, {
-        header = Lang:t('emotes.close'),
-    }}
-    for k, v in pairs(Config.Emotes.Reactions) do
-        EmoteMenu[#EmoteMenu + 1] = {
-            header = "/"..k,
-            txt = v.desc,
-            params = {
-                event = 'emotes:client:doemotemenu',
-                args = {
-                    emote = v.anim,
-                }
-            }
+    local options = {}
+    for k,v in pairs(Config.Emotes.Reactions) do
+        options[#options + 1] = {
+            title = "/"..k,
+            description = v.desc,
+            icon = 'fa-solid fa-icons',
+            event = 'emotes:client:doemotemenu',
+            args = { emote = v.anim },
+            arrow = false,
         }
     end
-    exports['rsg-menu']:openMenu(EmoteMenu)
+    lib.registerContext({
+        id = 'emotes_reactionmenu',
+        title = Lang:t('emotes.reaction.secondMenu'),
+        menu = 'emotes_mainmenu',
+        onBack = function() end,
+        position = 'top-right',
+        options = options
+    })
+    lib.showContext('emotes_reactionmenu')
 end)
 
+-------------------------------------------------------
+-- taunt emote menu
+-------------------------------------------------------
 RegisterNetEvent('emotes:client:tauntemotes', function()
-    Citizen.Wait(500)
-    local EmoteMenu = {{
-        header = Lang:t('emotes.taunting.secondMenu'),
-        isMenuHeader = true -- Set to true to make a nonclickable title
-    }, {
-        header = Lang:t('emotes.close'),
-    }}
-    for k, v in pairs(Config.Emotes.Taunting) do
-        EmoteMenu[#EmoteMenu + 1] = {
-            header = "/"..k,
-            txt = v.desc,
-            params = {
-                event = 'emotes:client:doemotemenu',
-                args = {
-                    emote = v.anim,
-                }
-            }
+    local options = {}
+    for k,v in pairs(Config.Emotes.Taunting) do
+        options[#options + 1] = {
+            title = "/"..k,
+            description = v.desc,
+            icon = 'fa-solid fa-icons',
+            event = 'emotes:client:doemotemenu',
+            args = { emote = v.anim },
+            arrow = false,
         }
     end
-    exports['rsg-menu']:openMenu(EmoteMenu)
+    lib.registerContext({
+        id = 'emotes_tauntmenu',
+        title = Lang:t('emotes.taunting.secondMenu'),
+        menu = 'emotes_mainmenu',
+        onBack = function() end,
+        position = 'top-right',
+        options = options
+    })
+    lib.showContext('emotes_tauntmenu')
 end)
 
-RegisterNetEvent('emotes:client:danceemotes', function()
-    Citizen.Wait(500)
-    local EmoteMenu = {{
-        header = Lang:t('emotes.dancing.secondMenu'),
-        isMenuHeader = true -- Set to true to make a nonclickable title
-    }, {
-        header = Lang:t('emotes.close'),
-    }}
-    for k, v in pairs(Config.Emotes.Dancing) do
-        EmoteMenu[#EmoteMenu + 1] = {
-            header = "/"..k,
-            txt = v.desc,
-            params = {
-                event = 'emotes:client:dodictemote',
-                args = {
-                    dict = v.dict,
-                    anim = v.anim,
-                    duration = v.duration,
-                }
-            }
+-------------------------------------------------------
+-- dancing emote menu
+-------------------------------------------------------
+RegisterNetEvent('emotes:client:dancingemotes', function()
+    local options = {}
+    for k,v in pairs(Config.Emotes.Dancing) do
+        options[#options + 1] = {
+            title = "/"..k,
+            description = v.desc,
+            icon = 'fa-solid fa-icons',
+            event = 'emotes:client:dodictemote',
+            args = {
+                dict = v.dict,
+                anim = v.anim,
+                duration = v.duration,
+            },
+            arrow = false,
         }
     end
-    exports['rsg-menu']:openMenu(EmoteMenu)
+    lib.registerContext({
+        id = 'emotes_dancingmenu',
+        title = Lang:t('emotes.dancing.secondMenu'),
+        menu = 'emotes_mainmenu',
+        onBack = function() end,
+        position = 'top-right',
+        options = options
+    })
+    lib.showContext('emotes_dancingmenu')
 end)
