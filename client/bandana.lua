99a536dea0576c1c54bf85b3284921c03499e91b
RexshackGaming <rexshackuk@gmail.com>
2024-04-27 00:22:47 +0100

    bandana ox_lib and natives update

--- client/bandana.lua
+++ client/bandana.lua
@@ -2,29 +2,26 @@ local bandana = false
 
 RegisterNetEvent('rsg-bandana:client:ToggleBandana')
 AddEventHandler('rsg-bandana:client:ToggleBandana', function()
-    local ped = PlayerPedId()
-    local male = IsPedMale(ped)
+    local male = IsPedMale(cache.ped)
     local neckwear = exports['rsg-appearance']:GetClothesCurrentComponentHash('neckwear')
-
-    -- Beard of Power: https://www.nexusmods.com/skyrimspecialedition/mods/42635
-    -- local beardofpower = exports['rsg-appearance']:GetBodyCurrentComponentHash('beard')
+    local applybeard = exports['rsg-appearance']:GetBodyCurrentComponentHash('beard')
 
     if not bandana then
         if not male then
-            Citizen.InvokeNative(0xAE72E7DF013AAA61, ped, `KIT_BANDANA`, `BANDANA_ON_RIGHT_HAND`, 1, 0, -1082130432)
+            StartTaskItemInteraction(cache.ped, `KIT_BANDANA`, `BANDANA_ON_RIGHT_HAND`, 1, 0, -1082130432)
             Wait(700)
-            Citizen.InvokeNative(0xD3A7B003ED343FD9, ped, 0xC615A086, true, true, true)
-            Citizen.InvokeNative(0xCC8CA3E88256E58F, ped, 0, 1, 1, 1, false)
+            ApplyShopItemToPed(cache.ped, 0xC615A086, true, true, true)
+            UpdatePedVariation(cache.ped, 0, 1, 1, 1, false)
 
             bandana = true
 
             return
         end
 
-        Citizen.InvokeNative(0xAE72E7DF013AAA61, ped, `KIT_BANDANA`, `BANDANA_ON_RIGHT_HAND`, 1, 0, -1082130432)
+        StartTaskItemInteraction(cache.ped, `KIT_BANDANA`, `BANDANA_ON_RIGHT_HAND`, 1, 0, -1082130432)
         Wait(700)
-        Citizen.InvokeNative(0x1902C4CFCC5BE57C, ped, -1100875244)
-        Citizen.InvokeNative(0xCC8CA3E88256E58F, ped, 0, 1, 1, 1, false)
+        EquipMetaPedOutfit(cache.ped, -1100875244)
+        UpdatePedVariation(cache.ped, 0, 1, 1, 1, false)
 
         bandana = true
 
@@ -32,25 +29,25 @@ AddEventHandler('rsg-bandana:client:ToggleBandana', function()
     end
 
     if not male then
-        Citizen.InvokeNative(0xAE72E7DF013AAA61, ped, `KIT_BANDANA`, `BANDANA_OFF_RIGHT_HAND`, 1, 0, -1082130432)
+        StartTaskItemInteraction(cache.ped, `KIT_BANDANA`, `BANDANA_OFF_RIGHT_HAND`, 1, 0, -1082130432)
         Wait(700)
-        Citizen.InvokeNative(0xD710A5007C2AC539, ped, 0x5FC29285, 0)
+        RemoveTagFromMetaPed(cache.ped, 0x5FC29285, 0)
         Wait(100)
-        Citizen.InvokeNative(0xD3A7B003ED343FD9, ped, neckwear, true, true, true)
-        Citizen.InvokeNative(0xCC8CA3E88256E58F, ped, 0, 1, 1, 1, false)
+        ApplyShopItemToPed(cache.ped, neckwear, true, true, true)
+        UpdatePedVariation(cache.ped, 0, 1, 1, 1, false)
 
         bandana = false
 
         return
     end
 
-    Citizen.InvokeNative(0xAE72E7DF013AAA61, ped, `KIT_BANDANA`, `BANDANA_OFF_RIGHT_HAND`, 1, 0, -1082130432)
+    StartTaskItemInteraction(cache.ped, `KIT_BANDANA`, `BANDANA_OFF_RIGHT_HAND`, 1, 0, -1082130432)
     Wait(700)
-    Citizen.InvokeNative(0xD710A5007C2AC539, ped, 0x5FC29285, 0)
+    RemoveTagFromMetaPed(cache.ped, 0x5FC29285, 0)
     Wait(100)
-    Citizen.InvokeNative(0xD710A5007C2AC539, ped, 0xF8016BCA, 0)
-    Citizen.InvokeNative(0xD3A7B003ED343FD9, ped, beardofpower, true, true, true)
-    Citizen.InvokeNative(0xCC8CA3E88256E58F, ped, 0, 1, 1, 1, false)
+    RemoveTagFromMetaPed(cache.ped, 0xF8016BCA, 0)
+    ApplyShopItemToPed(cache.ped, applybeard, true, true, true)
+    UpdatePedVariation(cache.ped, 0, 1, 1, 1, false)
 
     bandana = false
 end)
