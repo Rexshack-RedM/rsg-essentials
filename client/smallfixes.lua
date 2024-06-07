194663012bdec4c80b3e0b6a885ab75551a9e6be
RexshackGaming <rexshackuk@gmail.com>
2024-06-07 07:24:38 +0100

    removed friendly toggle

--- client/smallfixes.lua
+++ client/smallfixes.lua
@@ -85,39 +85,3 @@ Citizen.CreateThread(function()
         DisableControlAction(0, 0xCF8A4ECA, true) -- disable left alt hud | LEFT ALT (fast tapping)
     end
 end)
-
------------------------------
--- handle getting on other players horse (make friendly)
------------------------------
-CreateThread(function()
-    local active = false
-    local timer = 0
-
-    while true do 
-
-        Wait(0)
-
-        if IsControlJustPressed(0, 0xCEFD9220) then -- E KEY
-            timer = 0
-            active = true
-            while  timer < 200 do 
-                Wait(0)
-                timer = timer + 1
-                SetRelationshipBetweenGroups(1, `PLAYER`, `PLAYER`)
-            end
-            active = false
-        end
-
-        if IsControlJustPressed(0, 0xB2F377E8) then -- F KEY
-            Wait(500)
-            SetRelationshipBetweenGroups(1, `PLAYER`, `PLAYER`)
-            active = false
-            timer = 0
-        end
-
-        if active == false and not IsPedOnMount(PlayerPedId()) and not IsPedInAnyVehicle(PlayerPedId()) then
-            SetRelationshipBetweenGroups(5, `PLAYER`, `PLAYER`)
-        end
-
-    end
-end)
