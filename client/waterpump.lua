d5222e044375addd280be22929fe034226f93e42
Sadicius <124639760+Sadicius@users.noreply.github.com>
2024-08-04 06:41:07 +0200

    Update waterpump.lua
    CreateThread(function()
    end)
    
    in exports target

--- client/waterpump.lua
+++ client/waterpump.lua
@@ -6,38 +6,40 @@ WaterOutlet = {
     -717759843, -- p_wellpumpnbx01x
 }
 
-if Config.canteenPump then
-    exports['rsg-target']:AddTargetModel(WaterOutlet, {
-        options = {
-            {
-                type = "client",
-                event = 'rsg-waterpump:client:drinking',
-                icon = "far fa-eye",
-                label = Lang:t('label.take_a_drink'),
-                distance = 2.0
-            },
-            {
-                type = "client",
-                event = 'rsg-waterpump:client:canteenfill',
-                icon = "far fa-eye",
-                label = 'Fill up your canteen',
-                distance = 2.0
+CreateThread(function()
+    if Config.canteenPump then
+        exports['rsg-target']:AddTargetModel(WaterOutlet, {
+            options = {
+                {
+                    type = "client",
+                    event = 'rsg-waterpump:client:drinking',
+                    icon = "far fa-eye",
+                    label = Lang:t('label.take_a_drink'),
+                    distance = 2.0
+                },
+                {
+                    type = "client",
+                    event = 'rsg-waterpump:client:canteenfill',
+                    icon = "far fa-eye",
+                    label = 'Fill up your canteen',
+                    distance = 2.0
+                }
             }
-        }
-    })
-else
-    exports['rsg-target']:AddTargetModel(WaterOutlet, {
-        options = {
-            {
-                type = "client",
-                event = 'rsg-waterpump:client:drinking',
-                icon = "far fa-eye",
-                label = Lang:t('label.take_a_drink'),
-                distance = 2.0
+        })
+    else
+        exports['rsg-target']:AddTargetModel(WaterOutlet, {
+            options = {
+                {
+                    type = "client",
+                    event = 'rsg-waterpump:client:drinking',
+                    icon = "far fa-eye",
+                    label = Lang:t('label.take_a_drink'),
+                    distance = 2.0
+                }
             }
-        }
-    })
-end
+        })
+    end
+end)
 
 RegisterNetEvent('rsg-waterpump:client:canteenfill', function()
     if Config.canteenPump then
