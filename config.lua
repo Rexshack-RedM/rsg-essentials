adc2d51df39a741ff29555fa12931b7bbcbedce6
RexshackGaming <rexshackuk@gmail.com>
2024-10-03 22:00:36 +0100

    remove consume

--- config.lua
+++ config.lua
@@ -17,28 +17,6 @@ Config.StopAutoShuffle = true
 --can players fill the canteen from a water pump
 Config.canteenPump = true
 
--- consumables config
-Config.Consumables = {
-    Eat = { -- default food items
-        ['bread'] = {
-            item = 'bread',
-            hunger = 25,
-            thirst = 0,
-            stress = 5,
-            propname = 'p_bread_14_ab_s_a'
-        },
-    },
-    Drink = { -- default drink items
-        ['water'] = {
-            item = 'water',
-            hunger = 0,
-            thirst = 25,
-            stress = 5,
-            propname = 'p_bottlebeer01a'
-        },
-    },
-}
-
 -- Go to Discord Developers Portal (https://discord.com/developers)
 
 Config.Discord = {
