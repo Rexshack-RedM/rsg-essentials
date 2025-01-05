375cda546456bab81c71386490b52409a97e2d31
Artmines <96462463+Artmines@users.noreply.github.com>
2025-01-05 09:08:13 -0700

    Update eagleeye.lua

--- client/eagleeye.lua
+++ client/eagleeye.lua
@@ -1,12 +1,32 @@
+local RSGCore = exports['rsg-core']:GetCoreObject()
+
 function EnableEagleeye(player, enable)
     Citizen.InvokeNative(0xA63FCAD3A6FEC6D2, player, enable)
 end
 
+function ShouldEnableEagleEye(job)
+    if Config.EagleEye.everyone.enabled then
+        return true
+    end
+
+    if Config.EagleEye[job] and Config.EagleEye[job].enabled then
+        return true
+    end
+
+    return false
+end
+
+
 AddEventHandler('RSGCore:Client:OnPlayerLoaded', function()
-    if Config.EnableEagleEye then
+    local playerJob = RSGCore.Functions.GetPlayerData().job.name 
+    print("Player Job: " .. playerJob) 
+    if ShouldEnableEagleEye(playerJob) then
         EnableEagleeye(PlayerId(), true)
+    else
+        EnableEagleeye(PlayerId(), false)
     end
 end)
+
 
 AddEventHandler("onResourceStop", function(resourceName)
     if GetCurrentResourceName() ~= resourceName then
