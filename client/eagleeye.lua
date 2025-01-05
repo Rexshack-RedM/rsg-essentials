6c8c3fb4f524fe552c58e2646054af6495b39105
Artmines <96462463+Artmines@users.noreply.github.com>
2025-01-05 09:27:58 -0700

    Refresh on job change

--- client/eagleeye.lua
+++ client/eagleeye.lua
@@ -1,8 +1,11 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
+
 function EnableEagleeye(player, enable)
     Citizen.InvokeNative(0xA63FCAD3A6FEC6D2, player, enable)
+    print(enable and "Eagle Eye Enabled" or "Eagle Eye Disabled")
 end
+
 
 function ShouldEnableEagleEye(job)
     if Config.EagleEye.everyone.enabled then
@@ -16,18 +19,36 @@ function ShouldEnableEagleEye(job)
     return false
 end
 
-
-AddEventHandler('RSGCore:Client:OnPlayerLoaded', function()
-    local playerJob = RSGCore.Functions.GetPlayerData().job.name 
-    print("Player Job: " .. playerJob) 
+function HandleEagleEyeAccess()
+    local playerJob = RSGCore.Functions.GetPlayerData().job.name
     if ShouldEnableEagleEye(playerJob) then
         EnableEagleeye(PlayerId(), true)
     else
         EnableEagleeye(PlayerId(), false)
     end
+end
+
+
+AddEventHandler('RSGCore:Client:OnPlayerLoaded', function()
+    HandleEagleEyeAccess()
 end)
 
 
+RegisterNetEvent('RSGCore:Client:OnJobUpdate')
+AddEventHandler('RSGCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerJob = JobInfo
+    HandleEagleEyeAccess()
+end)
+
+
+AddEventHandler("onResourceStart", function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then
+        return
+    end
+    HandleEagleEyeAccess() 
+end)
+
+-- Event handler for when the resource stops
 AddEventHandler("onResourceStop", function(resourceName)
     if GetCurrentResourceName() ~= resourceName then
         return
