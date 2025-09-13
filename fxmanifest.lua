e1847388b1df92c5cbd638ea232cbae447d429c0
RexShack <rexshackuk@gmail.com>
2025-09-13 13:28:46 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 
 description 'rsg-essentials'
-version '2.3.6'
+version '2.3.7'
 
 shared_scripts {
     '@ox_lib/init.lua',
