0a1a14f8b6ae4b3cefa733e3a95b559ed619386a
RexShack <rexshackuk@gmail.com>
2025-12-20 13:59:48 +0000

    addtional inventory weapon check

--- client/weaponcheck.lua
+++ client/weaponcheck.lua
@@ -1,3 +1,5 @@
+local RSGCore = exports['rsg-core']:GetCoreObject()
+
 RegisterNetEvent('rsg-core:client:RemoveWeaponFromTab', function(weaponName)
     local ped = PlayerPedId()
     local weaponHash = GetHashKey(weaponName)
@@ -13,5 +15,25 @@ RegisterNetEvent('rsg-core:client:RemoveWeaponFromTab', function(weaponName)
 
     if weaponHash == weapon then 
         SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+    end
+end)
+
+CreateThread(function()   
+    while true do
+        Wait(1000)
+        local player = PlayerPedId()
+        local weapon = Citizen.InvokeNative(0x8425C5F057012DAB, player) -- GET_CURRENT_PED_WEAPON
+        local WeaponData = RSGCore.Shared.Weapons[weapon]
+        if WeaponData and WeaponData["name"] ~= "weapon_unarmed" then
+            local weaponGroup = Citizen.InvokeNative(0xEDCA14CA5199FF25, weapon) -- GET_WEAPONTYPE_GROUP
+            if weaponGroup ~= `group_thrown` then
+                local hasItem = RSGCore.Functions.HasItem(WeaponData["name"])
+                if not hasItem then
+                    -- remove weapon that is not in the inventory of the player
+                    SetCurrentPedWeapon(player, `WEAPON_UNARMED`, true)
+                    RemoveWeaponFromPed(player, weapon)
+                end
+            end
+        end
     end
 end)
